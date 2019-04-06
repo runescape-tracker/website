d75218677a8ebd97722293640f6382fb1f2c9b6f
Ian <hello@ian.pe>
2019-04-07 00:18:07 +0100

    update player profiles

--- resources/views/profile/rs3/index.blade.php
+++ resources/views/profile/rs3/index.blade.php
@@ -8,9 +8,6 @@
             We are tracking this user
         @endif
 
-        @if(!$yesterdayTrack)
-            <div class="alert alert-info">We have just started tracking this user today, please come back tomorrow to see your gains</div>
-        @else
         <table class="table table-bordered table-hover">
             <thead>
             <tr>
@@ -29,13 +26,17 @@
                     <td>{{ $currentTrack->{strtolower($skill) . "_level"} }}</td>
                     <td>{{ $currentTrack->{strtolower($skill) . "_xp"} }}</td>
                     <td>{{ $currentTrack->{strtolower($skill) . "_rank"} }}</td>
+                    @if(!$yesterdayTrack)
+                        <td>-</td>
+                        <td>--</td>
+                    @else
                     <td class="@if($currentTrack->{strtolower($skill) . "_xp"} - $yesterdayTrack->{strtolower($skill) . "_xp"} > 0) gained @endif">{{ $currentTrack->{strtolower($skill) . "_xp"} - $yesterdayTrack->{strtolower($skill) . "_xp"} }}</td>
                     <td class="@if($yesterdayTrack->{strtolower($skill) . "_xp"} - ($dayBeforeYesterdayTrack !== null ? $dayBeforeYesterdayTrack->{strtolower($skill) . "_xp"} : 0) > 0) gained @endif">{{ $yesterdayTrack->{strtolower($skill) . "_xp"} - ($dayBeforeYesterdayTrack !== null ? $dayBeforeYesterdayTrack->{strtolower($skill) . "_xp"} : 0) }}</td>
+                    @endif
                 </tr>
             @endforeach
             </tbody>
         </table>
-        @endif
     </div>
 
 
